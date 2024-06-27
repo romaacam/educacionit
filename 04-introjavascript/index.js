72bb9b79e761ccb8ddc7f6e1a9f4585db4add9fe
David <romaacam1@gmail.com>
2024-06-27 19:32:59 -0300

    se agrego data del profe

---
+++ 04-introjavascript/index.js
@@ -0,0 +1,30 @@
+
+
+// estos son los comentarios de una sola linea en javascript
+
+/* 
+Comentarios
+de
+varias
+lineas
+en JS
+*/
+
+/*
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+*/
+
+//Variables: espacios de memoria para almacenar datos
+//1. var  (palabra reservada)
+// palabras reservadas: son utilizadas sólo para las definiciones del lenguaje de programación
+var nombre;
+var email
+var edad;
+var pais
