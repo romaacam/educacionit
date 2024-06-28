10b582596d0ab62f3d8b56b6ef996062d87fd7d0
David <romaacam1@gmail.com>
2024-06-27 21:26:48 -0300

    se termina la clase 15

--- 04-introjavascript/prompt.js
+++ 04-introjavascript/prompt.js
@@ -3,6 +3,10 @@
 
 // El usuario nos proporciona los valores de los numeros 
 
+alert("Hola, vamos a sumar dos números")
+
+confirm("Este es un sitio de prueba, esta de acuerdo con eso?")
+
 //el prompt es una funcion que nos permite pedirle al use que ingrese un valor
 
 let numeroUnoDelUsuario = prompt('Ingresa el primer número: ')
